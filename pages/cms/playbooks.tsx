ce1ab4c9a4920817228bb8c62e00c233065a0930
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 16:31:18 -0700

    replaced many inputs with OptionInput

--- pages/cms/playbooks.tsx
+++ pages/cms/playbooks.tsx
@@ -1,23 +1,27 @@
+import { useState } from "react";
+import OptionInput from "../../components/OptionInput"
+
 const Playbooks = () => {
+    const [animals, setAnimals] = useState([])
+    const [personality, setPersonality] = useState([])
+    const [appearance, setAppearance] = useState([])
+    const [history, setHistory] = useState([])
+    const [relationships, setRelationships] = useState([])
+    const [signatureMoves, setSignatureMoves] = useState([])
+    const [seasonalMoves, setSeasonalMoves] = useState([])
+
     return <form>
         <label htmlFor="name">Playbook Name</label>
         <input id="name"></input>
         <label htmlFor="description">Description</label>
         <input id="description"></input>
-        <label htmlFor="animal">Animal</label>
-        <input id="animal"></input>
-        <label htmlFor="personality">Personality</label>
-        <input id="personality"></input>
-        <label htmlFor="appearance">Appearance</label>
-        <input id="appearance"></input>
-        <label htmlFor="history">History</label>
-        <input id="history"></input>
-        <label htmlFor="relationships">Relationships</label>
-        <input id="relationships"></input>
-        <label htmlFor="signatureMoves">Signature Moves</label>
-        <input id="signatureMoves"></input>
-        <label htmlFor="seasonalMoves">Seasonal Moves</label>
-        <input id="seasonalMoves"></input>
+        <OptionInput name="animal" submittedData={animals} setSubmitted={setAnimals} />
+        <OptionInput name="personality" submittedData={personality} setSubmitted={setPersonality} />
+        <OptionInput name="appearance" submittedData={appearance} setSubmitted={setAppearance} />
+        <OptionInput name="history" submittedData={history} setSubmitted={setHistory} />
+        <OptionInput name="relationships" submittedData={relationships} setSubmitted={setRelationships} />
+        <OptionInput name="signatureMoves" submittedData={signatureMoves} setSubmitted={setSignatureMoves} />
+        <OptionInput name="seasonalMoves" submittedData={seasonalMoves} setSubmitted={setSeasonalMoves} />
     </form>
 }
 
