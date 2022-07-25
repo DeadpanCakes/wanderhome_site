2c194a18138d68812680c5bc954ac828e94cdc54
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 15:34:32 -0700

    made page for admin login

---
+++ pages/admin.tsx
@@ -0,0 +1,12 @@
+const Admin = () => {
+    return (
+        <form>
+            <label htmlFor="username">Username</label>
+            <input id="username"></input>
+            <label htmlFor="password">Password</label>
+            <input id="password"></input>
+        </form>
+    )
+}
+
+export default Admin
