d084dba25f4b2d356c8026cda31246ad6beb7dda
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 15:57:49 -0700

    added layout for playbook form

---
+++ pages/cms/playbooks.tsx
@@ -0,0 +1,24 @@
+const Playbooks = () => {
+    return <form>
+        <label htmlFor="name">Playbook Name</label>
+        <input id="name"></input>
+        <label htmlFor="description">Description</label>
+        <input id="description"></input>
+        <label htmlFor="animal">Animal</label>
+        <input id="animal"></input>
+        <label htmlFor="personality">Personality</label>
+        <input id="personality"></input>
+        <label htmlFor="appearance">Appearance</label>
+        <input id="appearance"></input>
+        <label htmlFor="history">History</label>
+        <input id="history"></input>
+        <label htmlFor="relationships">Relationships</label>
+        <input id="relationships"></input>
+        <label htmlFor="signatureMoves">Signature Moves</label>
+        <input id="signatureMoves"></input>
+        <label htmlFor="seasonalMoves">Seasonal Moves</label>
+        <input id="seasonalMoves"></input>
+    </form>
+}
+
+export default Playbooks
