43e38815c540ead88e3c99532dc99e3caf7279e0
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 16:49:30 -0700

    added name props for post

--- pages/cms/playbooks.tsx
+++ pages/cms/playbooks.tsx
@@ -10,11 +10,11 @@ const Playbooks = () => {
     const [signatureMoves, setSignatureMoves] = useState([])
     const [seasonalMoves, setSeasonalMoves] = useState([])
 
-    return <form>
+    return <form method="POST" action={"/api/playbooks"}>
         <label htmlFor="name">Playbook Name</label>
-        <input id="name"></input>
+        <input id="name" name="name"></input>
         <label htmlFor="description">Description</label>
-        <input id="description"></input>
+        <input id="description" name="description"></input>
         <OptionInput name="animal" submittedData={animals} setSubmitted={setAnimals} />
         <OptionInput name="personality" submittedData={personality} setSubmitted={setPersonality} />
         <OptionInput name="appearance" submittedData={appearance} setSubmitted={setAppearance} />
@@ -22,6 +22,7 @@ const Playbooks = () => {
         <OptionInput name="relationships" submittedData={relationships} setSubmitted={setRelationships} />
         <OptionInput name="signatureMoves" submittedData={signatureMoves} setSubmitted={setSignatureMoves} />
         <OptionInput name="seasonalMoves" submittedData={seasonalMoves} setSubmitted={setSeasonalMoves} />
+        <button>Submit</button>
     </form>
 }
 
