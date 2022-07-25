f0d8d6f318ac12acdd47ec2d65e8484ca15ab40a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 16:27:04 -0700

    made component to render a set of data as you input new data

---
+++ components/OptionInput.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import { v4 as uuid } from "uuid"
+
+const OptionInput = ({ name, submittedData, setSubmitted }) => {
+    const [newData, setNewData] = useState("")
+    return (
+        <>
+            <label htmlFor={name}>{name}</label>
+            <ul>
+                {submittedData.map((i) => {
+                    return <li key={uuid()}>{i}</li>
+                })}
+                <div>
+                    <input id={name} value={newData} onChange={(e) => setNewData(e.target.value)}></input>
+                    <button onClick={(e) => {
+                        e.preventDefault()
+                        setSubmitted(prevState => prevState.concat(newData))
+                        setNewData("")
+                    }}>+</button>
+                </div>
+            </ul>
+        </>
+    )
+}
+
+export default OptionInput
