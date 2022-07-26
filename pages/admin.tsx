b4847be54ef3ff8a33f385a330e21445ab4b6338
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-26 13:25:21 -0700

    filled out admin site and use it to post to login api route

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,41 @@
-const Admin = () => {
-    return (
-        <form>
-            <label htmlFor="username">Username</label>
-            <input id="username"></input>
-            <label htmlFor="password">Password</label>
-            <input id="password"></input>
-        </form>
-    )
-}
+import { useState } from "react";
 
-export default Admin
+const Admin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await fetch("/api/login", {
+          method: "POST",
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+        const data = await res.json();
+        console.log(data);
+      }}
+    >
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        name="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      ></input>
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Admin;
