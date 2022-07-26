2a705ae0413f5f9f6d99fafad063fdb21e04d2d5
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 17:40:22 -0700

    made a route for adding traits

---
+++ pages/cms/traits/add.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import OptionInput from "../../../components/OptionInput";
+
+const Add = () => {
+  const [moves, setMoves] = useState([]);
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input id="name" name="name"></input>
+      <label htmlFor="description">Description</label>
+      <input id="description" name="description"></input>
+      <OptionInput name="moves" submittedData={moves} setSubmitted={setMoves} />
+    </form>
+  );
+};
+
+export default Add;
