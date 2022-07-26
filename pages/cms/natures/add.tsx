2b2a97be0bdb03a84b059fd65b0bc40fac80a04a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 17:44:50 -0700

    added page for adding natures

---
+++ pages/cms/natures/add.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import OptionInput from "../../../components/OptionInput";
+
+const Add = () => {
+  const [moves, setMoves] = useState([]);
+  const [aesthetics, setAesthetics] = useState([]);
+  const [folklore, setFolklore] = useState([]);
+  return (
+    <form>
+      <label htmlFor="name">Name</label>
+      <input id="name" name="name"></input>
+      <label htmlFor="description"></label>
+      <input id="description" name="description"></input>
+      <OptionInput name="moves" submittedData={moves} setSubmitted={setMoves} />
+      <OptionInput
+        name="aethetics"
+        submittedData={aesthetics}
+        setSubmitted={setAesthetics}
+      />
+      <OptionInput
+        name="folklore"
+        submittedData={folklore}
+        setSubmitted={setFolklore}
+      />
+    </form>
+  );
+};
+
+export default Add;
