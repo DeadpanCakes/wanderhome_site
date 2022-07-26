a2c3f7e98c5d7320d4384b620a1c090a71f2b19e
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 17:20:52 -0700

    added placeholder for index

---
+++ pages/cms/playbooks/index.tsx
@@ -0,0 +1,15 @@
+const Playbooks = () => {
+    return <>
+        <ul>
+            <li>Playbook 1</li>
+            <li>Playbook 2
+                <div>
+                    <button>+</button>
+                    <button>X</button>
+                </div>
+            </li>
+        </ul>
+    </>
+}
+
+export default Playbooks
