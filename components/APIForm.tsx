ba49ae11bf036c502f470dc714844b21d4fdeaca
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-28 15:33:51 -0700

    abstracted reusable functionality from trait form into APIForm

---
+++ components/APIForm.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import useError from "../hooks/useError";
+import Error from "./layouts/Error";
+
+const APIForm = ({ payload, url, method, children }) => {
+  const { error, setError: handleError, clearError } = useError();
+  const router = useRouter();
+
+  const handleSubmission = async () => {
+    const options = {
+      method,
+      body: JSON.stringify(payload),
+      headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
+    };
+    const response = await fetch(url, options);
+    if (networkFailure(response)) {
+      const error = await response.json();
+      handleError(error);
+    } else {
+      return response.json().then((data) => {
+        if (authFailure(data)) {
+          return handleError(data);
+        }
+        return router.reload();
+      });
+    }
+  };
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmission();
+      }}
+    >
+      <Error error={error} clearError={clearError} />
+      {children}
+    </form>
+  );
+};
+
+const networkFailure = (res) => res.status === 400;
+const authFailure = (data) => data.detail;
+
+export default APIForm;
--- components/TraitForm.tsx
+++ components/TraitForm.tsx
@@ -3,50 +3,25 @@ import { useRouter } from "next/router";
 import useError from "../hooks/useError";
 import Error from "../components/layouts/Error";
 import InputField from "./InputField";
+import APIForm from "./APIForm";
 
 const TraitForm = ({ category }) => {
   const [isMagic, setIsMagic] = useState(false);
   const toggleIsMagic = () => setIsMagic((prevState) => !prevState);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const { error, setError, clearError } = useError();
-  const handleErrors = (error) => setError(error);
-  const router = useRouter();
-
-  const submitTrait = async (info) => {
-    const url = "/api/traits/trait";
-    const options = {
-      body: JSON.stringify(info),
-      headers: { Authorization: "Bearer " + localStorage.getItem("jwt") },
-      method: "POST",
-    };
-    const response = await fetch(url, options);
-    if (networkFailure(response)) {
-      const error = await response.json();
-      handleErrors(error);
-    } else {
-      return response.json().then((data) => {
-        if (authFailure(data)) {
-          return handleErrors(data);
-        }
-        return router.reload();
-      });
-    }
-  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        submitTrait({
-          name,
-          description,
-          category: category.id,
-          is_magic: isMagic,
-        });
+    <APIForm
+      payload={{
+        is_magic: isMagic,
+        name,
+        description,
+        category: category.id,
       }}
+      url="/api/traits/trait"
+      method="POST"
     >
-      <Error error={error} clearError={clearError} />
       <label htmlFor="isMagic">Is Magical</label>
       <input
         id="isMagic"
@@ -62,11 +37,8 @@ const TraitForm = ({ category }) => {
         changeHandler={setDescription}
       />
       <button>Submit</button>
-    </form>
+    </APIForm>
   );
 };
-
-const networkFailure = (res) => res.status === 400;
-const authFailure = (data) => data.detail;
 
 export default TraitForm;
