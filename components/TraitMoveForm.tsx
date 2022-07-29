71ac11895bfbb832d96ccee0d7bb01d5ca34d6f1
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-28 17:05:01 -0700

    made form for TraitMoves

---
+++ components/TraitMoveForm.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import APIForm from "./APIForm";
+import BooleanField from "./BooleanField";
+import InputField from "./InputField";
+
+const TraitMoveForm = ({ trait }) => {
+  const [text, setText] = useState("");
+  const [isMagic, setIsMagic] = useState(false);
+  const toggleIsMagic = () => setIsMagic((prevState) => !prevState);
+  const [isTraumatized, setIsTraumatized] = useState(false);
+  const toggleIsTraumatized = () => setIsTraumatized((prevState) => !prevState);
+  const [nonMagicText, setNonMagicText] = useState("");
+  const [nonTraumatizedText, setNonTraumatizedText] = useState("");
+  const [nonTraumaNonMagicText, setNonTraumaNonMagicText] = useState("");
+
+  return (
+    <APIForm
+      url="/api/traits/moves"
+      method="POST"
+      payload={{ text, trait: trait.id }}
+    >
+      <BooleanField name="isMagic" boolean={isMagic} toggler={toggleIsMagic} />
+      <BooleanField
+        name="isTraumatized"
+        boolean={isTraumatized}
+        toggler={toggleIsTraumatized}
+      />
+      <InputField name="text" value={text} changeHandler={setText} />
+      {isMagic ? (
+        <InputField
+          name="nonMagicText"
+          value={nonMagicText}
+          changeHandler={setNonMagicText}
+        />
+      ) : null}
+      {isTraumatized ? (
+        <InputField
+          name="nonTraumaText"
+          value={nonTraumatizedText}
+          changeHandler={setNonTraumatizedText}
+        />
+      ) : null}
+      {isMagic && isTraumatized ? (
+        <InputField
+          name="nonMagicText"
+          value={nonTraumaNonMagicText}
+          changeHandler={setNonTraumaNonMagicText}
+        />
+      ) : null}
+      <button>Submit</button>
+    </APIForm>
+  );
+};
+
+export default TraitMoveForm;
