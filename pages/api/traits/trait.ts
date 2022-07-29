af263acce395c8630b5a3183374741b6bb4188ec
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-28 17:04:20 -0700

    imported APIhandler

--- pages/api/traits/trait.ts
+++ pages/api/traits/trait.ts
@@ -1,31 +1,17 @@
+import APIHandler from "../../../middleware/APIHandler"
+
 const handler = async (req, res) => {
+    const baseURL = process.env.API + "traits/"
+    const { authorization } = req.headers
+    const apiMethods = APIHandler(baseURL, req.body, authorization)
     if (req.method === "POST") {
-        const data = await postTrait(req.body, req.headers.authorization)
+        const data = apiMethods.post()
         return res.json(data)
     }
     if (req.method === "DELETE") {
-        const { id } = JSON.parse(req.body)
-        const data = await deleteTrait(id, req.headers.authorization)
-        if (data.status === 204) {
-            return res.json({ "message": "deleted" })
-        }
-        return res.json(await data.json())
+        const data = apiMethods.delete()
+        return res.json(data)
     }
-}
-
-const postTrait = async (info, auth) => {
-    const body = info
-    const url = process.env.API + "traits/"
-    const headers = { "Content-Type": "application/json", Authorization: auth }
-    const response = await fetch(url, { method: "POST", body, mode: "cors", headers })
-    return await response.json()
-}
-
-const deleteTrait = async (targetID, auth) => {
-    const url = process.env.API + `traits/${targetID}/`
-    console.log(url)
-    const headers = { "Content-Type": "application/json", Authorization: auth }
-    return await fetch(url, { method: "DELETE", mode: "cors", headers })
 }
 
 export default handler
