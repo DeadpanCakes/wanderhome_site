61700e2293b025f8619266d957a4407535d688ea
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-28 17:38:05 -0700

    made basic page for nature categories

---
+++ pages/cms/natures/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Natures = (props) => {
+  const categories = JSON.parse(props.categories);
+  return (
+    <>
+      <h1>Nature Categories</h1>
+      <h2>Natures</h2>
+      <ul>
+        {categories.map((category) => {
+          return (
+            <li key={category.id}>
+              <Link href={`/cms/natures/${category.name}`}>
+                <a>{category.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const url = process.env.API + "nature-categories/";
+  const response = await fetch(url);
+  const data = await response.json();
+  const categories = JSON.stringify(data);
+  return { props: { categories } };
+};
+
+export default Natures;
