8538beb9ccd4c708eb12b586728e4be716176a63
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-28 17:56:50 -0700

    made basic page for nature categories

---
+++ pages/cms/natures/[category]/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import ChildList from "../../../../components/ChildList";
+
+const Nature = (props) => {
+  const category = JSON.parse(props.category);
+  return (
+    <>
+      <h1>{category.name}</h1>
+      <ChildList
+        childArray={category.nature_set}
+        category="Natures"
+        baseURL={`/cms/natures/${category.name}/`}
+      />
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const { category: target } = context.params;
+  const url = process.env.API + "nature-categories/";
+  const response = await fetch(url);
+  const data = await response.json();
+  const targetCategory = data.find((category) => category.name === target);
+  return { props: { category: JSON.stringify(targetCategory) } };
+};
+
+export default Nature;
