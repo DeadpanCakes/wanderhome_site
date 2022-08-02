5350d180e3becaf9af188bb319b6e36afba97305
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-01 18:33:33 -0700

    finished making accordian

--- components/layouts/Accordian.tsx
+++ components/layouts/Accordian.tsx
@@ -1,20 +1,23 @@
-import { useState } from "react"
+import { useState } from "react";
+import styles from "../../styles/layouts/accordianLayout.module.css";
 
-interface Props {
-    children: React.ReactNode
-}
+const Accordian = ({ parent, children }) => {
+  console.log("mounted");
+  const [isExpanded, setExpansion] = useState(false);
+  const toggleExpansion = () => setExpansion((prevState) => !prevState);
+  return (
+    <div className={styles.accordian}>
+      <div className={styles.parent}>
+        {parent}
+        <button onClick={toggleExpansion}>{isExpanded ? "^" : "v"}</button>
+      </div>
+      <div
+        className={isExpanded ? styles.body : `${styles.body} ${styles.hidden}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
 
-const Accordian = ({ children }: Props) => {
-    const [isExpanded, setExpansion] = useState(false)
-    const toggleExpansion = () => setExpansion(prevState => !prevState)
-    return (
-        <div>
-            <div>
-                {children}
-            </div>
-            <button onClick={toggleExpansion}>{isExpanded ? "^" : "v"}</button>
-        </div>
-    )
-}
-
-export default Accordian
+export default Accordian;
