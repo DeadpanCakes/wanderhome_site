1b7672a803bfb7a79ad2f521376e125270c24cc9
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-01 17:41:44 -0700

    made playbook to render playbooks

---
+++ components/Playbook.tsx
@@ -0,0 +1,61 @@
+import ListLayout from "./layouts/ListLayout";
+import styles from "../styles/playbook.module.css";
+
+const Playbook = ({ playbook }) => {
+  const {
+    name,
+    description,
+    animals,
+    personality,
+    looks,
+    histories,
+    relationships,
+    signatureMoves,
+    seasonalMoves,
+  } = playbook;
+  console.log(playbook);
+  return (
+    <div className={styles.playbook}>
+      <div className={styles.header}>
+        <h1>{name}</h1>
+        <p>{description}</p>
+      </div>
+      <ListLayout
+        header={<h2>Choose an animal</h2>}
+        list={animals.map((animal) => animal.text)}
+      />
+      <ListLayout
+        header={<h2>{personality.prompt}</h2>}
+        list={personality.options.map((option) => option.text)}
+      />
+      <ListLayout
+        header={<h2>Choose 3-4 to describe your look</h2>}
+        list={looks.map((option) => option.text)}
+      />
+      {histories.map((history) => {
+        return (
+          <ListLayout
+            header={<h2>{history.prompt}</h2>}
+            list={history.options.map((option) => option.text)}
+          />
+        );
+      })}
+      <ListLayout
+        header={<h2>Ask 1 to the left and 1 to the right.</h2>}
+        list={relationships.map((relationship) => relationship.text)}
+      />
+      <ListLayout
+        header={<h2>Some things you can always do:</h2>}
+        list={signatureMoves.map((move) => move.text)}
+      />
+      <ListLayout
+        header={
+          <h2>During each holiday, choose 1 you haven't chosen before.</h2>
+        }
+        list={seasonalMoves.map((move) => move.text)}
+      />
+    </div>
+  );
+};
+
+export default Playbook;
