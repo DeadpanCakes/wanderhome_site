ca068574ae65830beb6c44fdd9e777a325629b49
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-05 16:59:31 -0700

    made route for making a new character

---
+++ pages/new/character.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import PCForm from "../../components/forms/PCForm";
+import useStorage from "../../hooks/useStorage";
+
+const character = (props) => {
+  const playbooks = JSON.parse(props.playbooks);
+  const [characters, setCharacter, fetchCharacter] = useStorage(
+    "characters",
+    null
+  );
+  useEffect(() => {
+    if (!character) {
+      fetchCharacter();
+    }
+  }, []);
+  const addCharacter = (newCharacter) => {
+    setCharacter((prevState) => {
+      if (prevState) {
+        return prevState.concat(newCharacter);
+      }
+      return [newCharacter];
+    });
+  };
+  return <PCForm playbooks={playbooks} submitHandler={addCharacter} />;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(process.env.API + "playbooks/");
+  const playbooks = await res.json();
+  return { props: { playbooks: JSON.stringify(playbooks) } };
+};
+
+export default character;
