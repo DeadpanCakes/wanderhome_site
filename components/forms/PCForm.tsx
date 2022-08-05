0cb7996510c6a68ddf971dac260a1b4e61908788
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-05 16:27:50 -0700

    validation now working

--- components/forms/PCForm.tsx
+++ components/forms/PCForm.tsx
@@ -37,16 +37,15 @@ const PCForm = ({ playbooks }) => {
     signature_move_set: [],
     seasonal_move_set: [],
   });
-  const personalityIsValid = () =>
+  const personalityIsValid =
     personality.positive.choices.length === 2 &&
     personality.negative.choices.length === 2;
-  const lookIsValid = () => 4 >= looks.length && looks.length >= 3;
+  const lookIsValid = 4 >= looks.length && looks.length >= 3;
+  const historyIsValid = histories.length === chosenPlaybook.history_set.length;
+  const relationshipIsValid = relationships.length === 2;
   const pageOneValid = useValidation(chosenPlaybook.id);
-  const pageTwoValid = useValidation(
-    animal,
-    personalityIsValid(),
-    lookIsValid()
-  );
+  const pageTwoValid = useValidation(animal, personalityIsValid, lookIsValid);
+  const pageThreeValid = useValidation(historyIsValid, relationshipIsValid);
   const submitHandler = console.log;
 
   const PageThree = (
@@ -59,7 +58,66 @@ const PCForm = ({ playbooks }) => {
               {history.option_set.map((option) => {
                 return (
                   <li key={option.id}>
-                    <input type="checkbox" id={option.id} />
+                    <input
+                      type="checkbox"
+                      id={option.id}
+                      onChange={() => {
+                        const chosenHistory = chosenPlaybook.history_set.find(
+                          (h) => h.id == history.id
+                        );
+                        const chosenOption = chosenHistory.option_set.find(
+                          (o) => o.id == option.id
+                        );
+                        const existingProp = histories.find(
+                          (h) => h.id == history.id
+                        );
+                        if (existingProp) {
+                          return setHistories((prevState) => {
+                            return prevState.map((h) => {
+                              if (h.id == existingProp.id) {
+                                if (
+                                  existingProp.choices.some(
+                                    (choice) => choice.id == option.id
+                                  )
+                                ) {
+                                  return {
+                                    ...h,
+                                    choices: h.choices.filter((o) => {
+                                      o.id != option.id;
+                                    }),
+                                  };
+                                }
+                                return {
+                                  ...h,
+                                  choices: h.choices.concat(chosenOption),
+                                };
+                              }
+                              return { ...h };
+                            });
+                          });
+                        }
+                        return setHistories((prevState) => {
+                          return prevState.concat({
+                            id: chosenHistory.id,
+                            prompt: chosenHistory.prompt,
+                            choices: [chosenOption],
+                          });
+                        });
+                        /*
+                        if you click on an option that belongs to a cat that already exists
+                          check if cat already exists
+                        
+                          Return identical state except that cat
+                          that cat will change one of two ways
+                            if that option already exists in that cat instance
+                              then filter it out
+                            else
+                              concatenate it
+                        else if it does no exist
+                          return identical state, but concat new instance
+                        */
+                      }}
+                    />
                     <label htmlFor={option.id}>{option.text}</label>
                   </li>
                 );
@@ -74,7 +132,26 @@ const PCForm = ({ playbooks }) => {
           {chosenPlaybook.relationship_set.map((relationship) => {
             return (
               <li>
-                <input type="checkbox" id={relationship.id} />
+                <input
+                  type="checkbox"
+                  id={relationship.id}
+                  onChange={() => {
+                    setRelationships((prevState) => {
+                      if (
+                        prevState.find((choice) => relationship.id == choice.id)
+                      ) {
+                        return prevState.filter(
+                          (choice) => choice.id == relationship.id
+                        );
+                      }
+                      return prevState.concat(
+                        chosenPlaybook.relationship_set.find(
+                          (option) => option.id == relationship.id
+                        )
+                      );
+                    });
+                  }}
+                />
                 <label htmlFor={relationship.id}>{relationship.text}</label>
               </li>
             );
@@ -82,6 +159,7 @@ const PCForm = ({ playbooks }) => {
         </ul>
       </div>
       <button
+        disabled={!pageThreeValid}
         onClick={() => {
           submitHandler({
             name,
@@ -107,7 +185,9 @@ const PCForm = ({ playbooks }) => {
         e.preventDefault();
       }}
     >
-      <button onClick={() => console.log(personality)}>Check</button>
+      <button onClick={() => console.log(histories, relationshipIsValid)}>
+        Check
+      </button>
       <div className={styles.basic}>
         <InputField name="name" value={name} changeHandler={setName} />
         <p>{chosenPlaybook.name.toUpperCase()}</p>
