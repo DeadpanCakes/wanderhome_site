f05003c48f5cafc7040e5f572c7c24e776be54e1
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-05 17:00:39 -0700

    reworked playercharcter to implement saved chars

--- components/PlayerCharacter.tsx
+++ components/PlayerCharacter.tsx
@@ -1,63 +1,16 @@
+import React from "react";
 import GenericMoves from "./GenericMoves";
 import ListLayout from "./layouts/ListLayout";
 import Accordian from "./layouts/Accordian";
 import styles from "../styles/PlayerCharacter.module.css";
 
-const PlayerCharacter = () => {
-  const character = {
-    name: "Ari",
-    class: "The Caretaker",
-    animal: "Stoat",
-    personality: {
-      positive: { prompt: "Values", choices: ["Expressive", "Patient"] },
-      negative: { prompt: "Exhausted by", choices: ["Alert", "Organized"] },
-    },
-    looks: [
-      "Huge Glasses",
-      "Ceremonial Robes",
-      "Flowing Dress",
-      "Constant Rythmic Tapping",
-    ],
-    histories: [
-      {
-        prompt:
-          "Choose up to 5 friends that hide in the many shrines you carry with you.",
-        choices: [
-          "Dulcet, a god of tiny melodies. They are dramatic and/or glamorous.",
-          "Furtive, a god of dust bunnies and cobwebs. They are quiet and/or invisible.",
-          "Guile, a god of Gateling winds. They are watchful and/or oracular.",
-          "Ia, a god of a misplaced kiss. They are caring and/or glamorous",
-          "Lilt, a god of a tossed-aside creature. They are nervous and/or dead.",
-        ],
-      },
-      {
-        prompt:
-          "Choose 1 ceremonial object you still honor and 1 you cannot treasure any longer. Tell the table about them.",
-        choices: [
-          "Honors: A box of beeswax candles, handmade by your learned mentor as part of their final lesson.",
-          "No longer Treasures: A beautiful wooden walking stick adorned with feathers and runes, passed down through generations.",
-        ],
-      },
-    ],
-    relationships: [
-      "Blank gave me Dulcet",
-      "I listen to Cecilia when she feels small and forgotten",
-    ],
-    signatureMoves: [
-      "Pause, tilt your head to the side, and keep going.",
-      "Play with one of your gods.",
-    ],
-    seasonalMoves: [
-      "Choose something a playbook can do. One of your gods learns how to do it.",
-      "Choose something a place's nature can do. One of your gods learns how to do it.",
-    ],
-  };
+const PlayerCharacter = ({ character }) => {
   return (
     <div>
       <Accordian
         parent={
           <h1>
-            {character.name}, {character.animal}. {character.class}
+            {character.name}, {character.animal}. {character.class.name}
           </h1>
         }
       >
@@ -65,7 +18,7 @@ const PlayerCharacter = () => {
           <Accordian parent={<h2>Appearance</h2>}>
             <ul>
               {character.looks.map((look) => {
-                return <li>{look}</li>;
+                return <li>{look.text}</li>;
               })}
             </ul>
           </Accordian>
@@ -73,13 +26,13 @@ const PlayerCharacter = () => {
             <h3>{character.personality.positive.prompt}:</h3>
             <ul>
               {character.personality.positive.choices.map((choice) => (
-                <li key={choice}>{choice}</li>
+                <li key={choice.text}>{choice.text}</li>
               ))}
             </ul>
             <h3>{character.personality.negative.prompt}:</h3>
             <ul>
               {character.personality.negative.choices.map((choice) => (
-                <li key={choice}>{choice}</li>
+                <li key={choice.text}>{choice.text}</li>
               ))}
             </ul>
           </Accordian>
@@ -88,13 +41,13 @@ const PlayerCharacter = () => {
       <GenericMoves />
       <ListLayout
         header={<h2>You Can Always:</h2>}
-        list={character.signatureMoves}
+        list={character.signatureMoves.map((move) => move.text)}
       />
       {character.histories.map((history) => {
         return (
           <ListLayout
             header={<h2>{history.prompt}</h2>}
-            list={history.choices}
+            list={history.choices.map((choice) => choice.text)}
           />
         );
       })}
@@ -104,7 +57,7 @@ const PlayerCharacter = () => {
             During each seasonal holiday, choose 1 you haven't chosen before.
           </h2>
         }
-        list={character.seasonalMoves}
+        list={character.seasonalMoves.map((move) => move.text)}
       />
     </div>
   );
