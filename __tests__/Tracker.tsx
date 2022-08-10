ce936e6feb039765bd7ad8404d57054233fea0c8
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-10 14:34:21 -0700

    added test for optional threshold  prop

--- __tests__/Tracker.tsx
+++ __tests__/Tracker.tsx
@@ -32,3 +32,19 @@ it("can reset the count", async () => {
   await user.click(screen.getByTitle("initCount"));
   expect(input.value).toEqual("0");
 });
+
+it("tracks when a counter reaches its upper limit", async () => {
+  const tokenName = "Raindrop";
+  const user = userEvent.setup();
+  const threshhold = 8;
+  render(<Tracker tokenName={tokenName} threshhold={threshhold} />);
+  const input: HTMLInputElement = screen.getByLabelText(tokenName, {
+    exact: false,
+  });
+  let i = 0;
+  while (i <= threshhold) {
+    await user.click(screen.getByText("+"));
+    i++;
+  }
+  expect(input.value).toEqual(threshhold.toString());
+});
