2c2602e80201cf13ee99fc54622bb33105e222e6
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-11 17:00:45 -0700

    imported React for testing

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import React, { useState } from "react";
 import useAuth from "../hooks/useAuth";
 
 const Admin = () => {
