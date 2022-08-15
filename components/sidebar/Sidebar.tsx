9d19230284817fb05584e89b46ffdc5032eaa9c2
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-15 15:53:46 -0700

    made basic sidebar element to hold some nav

---
+++ components/sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import AddBtn from "../AddBtn";
+import styles from "../../styles/sidebar/Sidebar.module.css";
+
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleSidebar = () => setCollapsed((prevState) => !prevState);
+  return (
+    <div
+      className={
+        collapsed ? styles.sidebar + " " + styles.hidden : styles.sidebar
+      }
+    >
+      <button className={styles.toggleBtn} onClick={toggleSidebar}>
+        X
+      </button>
+      <div className={styles.content}>
+        <AddBtn text="Make New Character" href="/new/character" />
+        <AddBtn text="Make New Place" href="/new/place" />
+        <AddBtn text="Make New Kith" href="/new/kith" />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
