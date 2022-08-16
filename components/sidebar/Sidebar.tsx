e20d9938a29966994d2e1902be83ee4b8fbfb0d9
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-16 15:52:35 -0700

    added a link back to home from any other page

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import AddBtn from "../AddBtn";
 import styles from "../../styles/sidebar/Sidebar.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(true);
   const toggleSidebar = () => setCollapsed((prevState) => !prevState);
+  const router = useRouter();
   return (
     <div
       className={
@@ -15,6 +18,11 @@ const Sidebar = () => {
         X
       </button>
       <div className={styles.content}>
+        {router.pathname === "/" ? null : (
+          <Link href="/">
+            <a>Back To Game</a>
+          </Link>
+        )}
         <AddBtn text="Make New Character" href="/new/character" />
         <AddBtn text="Make New Place" href="/new/place" />
         <AddBtn text="Make New Kith" href="/new/kith" />
