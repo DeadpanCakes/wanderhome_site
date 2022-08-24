c5c302bb2bf374797e8cf4ca3131ff48f57c2a65
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-23 17:25:29 -0700

    reworked GameContext to behave more functionally

--- components/context/GameContext.tsx
+++ components/context/GameContext.tsx
@@ -1,54 +1,11 @@
 import React, { useEffect, useState } from "react";
 import useStorage from "../../hooks/useStorage";
 
-const GameContext = React.createContext(null);
+const GameContext = React.createContext({
+  characters: null,
+  places: null,
+  kith: null,
+  counters: null,
+});
 
-export const GameContextProvider = ({ natures, months, children }) => {
-  const [characters, setCharacters, fetchCharacters] = useStorage(
-    "characters",
-    null
-  );
-  const [kith, setKith, fetchKith] = useStorage("npcs", null);
-  const [places, setPlaces, fetchPlaces] = useStorage("places", null);
-  const [lack, setLack] = useState({ id: null });
-  const [signs, setSigns] = useState([]);
-  const [activeMonth, setActiveMonth] = useState(months[0]);
-  const [activePlace, setActivePlace] = useState(null);
-  const [counters, setCounters, fetchCounters] = useStorage("counters", null);
-
-  useEffect(() => {
-    if (!characters) {
-      fetchCharacters();
-    }
-    if (!kith) {
-      fetchKith();
-    }
-    if (!places) {
-      fetchPlaces();
-    }
-    if (!counters) {
-      fetchCounters();
-    }
-  }, []);
-  useEffect(() => {
-    if (places) {
-      setActivePlace(places[0]);
-    }
-  }, [places]);
-  return (
-    <GameContext.Provider
-      value={{
-        characters,
-        kith,
-        places,
-        lack,
-        signs,
-        activeMonth,
-        activePlace,
-        counters,
-      }}
-    >
-      {children}
-    </GameContext.Provider>
-  );
-};
+export default GameContext;
