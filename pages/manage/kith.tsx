f95d2a1c40ad28945f3489e11c5e6a1cb4385939
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-24 14:22:11 -0700

    added delete functionality

--- pages/manage/kith.tsx
+++ pages/manage/kith.tsx
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import DefaultLayout from "../../components/layouts/DefaultLayout";
 import GameContext from "../../components/context/GameContext";
 import NPC from "../../components/NPC";
+import PopupLayout from "../../components/layouts/PopupLayout";
 
 const Kith = () => {
-  const { kith, activeKith, setActiveKith } = useContext(GameContext);
+  const { kith, setKith, activeKith, setActiveKith } = useContext(GameContext);
+  const [deleteTarget, setDeleteTarget] = useState(null);
   return (
     <DefaultLayout>
       <main>
@@ -13,6 +15,33 @@ const Kith = () => {
           Here, you can decide which Kith to display on your dashboard by
           checking their box, or delete them by clicking on the X.
         </h2>
+        <PopupLayout
+          isVisible={deleteTarget}
+          setVisibility={() => setDeleteTarget(null)}
+        >
+          {deleteTarget ? (
+            <>
+              <h2>
+                Are you sure you want to delete{" "}
+                {kith.find((k) => k.id === deleteTarget).name}?
+              </h2>
+              <button
+                onClick={() => {
+                  setKith((prevState) =>
+                    prevState.filter((p) => p.id !== deleteTarget)
+                  );
+                  setActiveKith((prevState) =>
+                    prevState.filter((p) => p !== deleteTarget)
+                  );
+                  setDeleteTarget(null);
+                }}
+              >
+                Yes
+              </button>
+              <button onClick={() => setDeleteTarget(null)}>No</button>
+            </>
+          ) : null}
+        </PopupLayout>
         <ul>
           {kith && activeKith
             ? kith.map((k) => {
@@ -20,7 +49,13 @@ const Kith = () => {
                   <li key={k.id}>
                     <div>
                       <button>Edit</button>
-                      <button>X</button>
+                      <button
+                        onClick={() => {
+                          setDeleteTarget(k.id);
+                        }}
+                      >
+                        X
+                      </button>
                     </div>
                     <input
                       type="checkbox"
