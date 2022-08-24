a1fdcd81b005a7a444c7b23582b4d9a8c2f00e7a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-24 13:47:13 -0700

    made manage page to allow for managing of game state

---
+++ pages/manage/index.tsx
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import Link from "next/link";
+import GameContext from "../../components/context/GameContext";
+import DefaultLayout from "../../components/layouts/DefaultLayout";
+
+const Manage = () => {
+  const {
+    characters,
+    places,
+    kith,
+    activeChar,
+    setActiveChar,
+    fetchActiveChar,
+    activePlace,
+    setActivePlace,
+    fetchActivePlace,
+    activeKith,
+    setActiveKith,
+    fetchActiveKith,
+  } = useContext(GameContext);
+  return (
+    <DefaultLayout>
+      <main>
+        {characters ? (
+          <SettingSelect options={characters} setChange={setActiveChar} />
+        ) : (
+          <p>No Chars Yet</p>
+        )}
+        {places ? (
+          <SettingSelect options={places} setChange={setActivePlace} />
+        ) : (
+          <p>No Places Yet</p>
+        )}
+        <Link href="/manage/kith">
+          <a>Manage Kith</a>
+        </Link>
+      </main>
+    </DefaultLayout>
+  );
+};
+
+const SettingSelect = ({ options, setChange }) => {
+  return (
+    <select onChange={(e) => setChange(e.target.value)}>
+      {options.map((o) => {
+        return (
+          <option value={o.id} key={o.id}>
+            {o.name}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default Manage;
