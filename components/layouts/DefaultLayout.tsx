66aa133faec059779868604fcd0316f3195ef45f
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-08-28 14:54:57 -0700

    applied theme to default layout

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "../header/Header";
 import Sidebar from "../sidebar/Sidebar";
 import Footer from "../footer/Footer";
+import ThemeContext from "../context/ThemeContext";
 
 const DefaultLayout = ({ children }) => {
+  const { activeTheme } = useContext(ThemeContext);
   return (
-    <>
+    <div
+      style={{ color: activeTheme.fore, background: activeTheme.backGradient }}
+    >
       <Header />
       <Sidebar />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
